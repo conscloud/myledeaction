88556fce9795f2c722196094f75fed4079a424b1
conscloud <conscloud@gmail.com>
2023-11-05 22:21:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,10 +17,10 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/feeds/luci/luci-thmem-opentomato
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/feeds/luci/luci-theme-opentomcat
-rm -rf package/feeds/luci/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
+# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/feeds/luci/luci-theme-opentomcat
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 #git clone https://github.com/vernesong/OpenClash.git 
-git clone --branch master --depth=1 https://github.com/vernesong/OpenClash.git package/lean/openclash
+git clone --branch master --depth=1 https://github.com/vernesong/OpenClash.git feeds/luci/applications/openclash
 #mv OpenClash/luci-app-openclash package/lean/
 #wget -P package/kernel/mac80211/patches/rt2x00/ https://raw.githubusercontent.com/WYC-2020/lede/893ba3d9e6984f90560a0f93921f651ee3ae96cf/package/kernel/mac80211/patches/rt2x00/651-rt2x00-driver-compile-with-kernel-5.15.patch 
